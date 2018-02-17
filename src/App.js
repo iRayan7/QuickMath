745b6a44d40e535d2a0d55423d3fbb93ec5766be
Rayan <ryn7des@gmail.com>
2018-02-17 21:59:25 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+    render(){
+        return (
+            <View style={styles.containerStyles}>
+
+            </View>
+        );
+    }
+
+}
+
+const styles = {
+    containerStyles: {
+        flex: 1,
+
+    }
+}
