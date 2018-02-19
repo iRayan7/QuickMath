949eef3db21b6479b87c6509f89b0aa81e35407e
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2018-02-19 12:47:11 +0300

    added first stage of the first mode

--- src/components/Mode1.js
+++ src/components/Mode1.js
@@ -4,11 +4,59 @@ import { Button, Icon } from 'react-native-elements';
 
 class Mode1 extends Component {
 
-    render(){
+    renderTureButton = () => {
         return (
-            <Text>Mode 1</Text>
+            <View style={{flex: 1}} >
+                <TouchableOpacity>
+                    <Image
+                        style={styles.ButtonStyles}
+                        source={require('./images/true.png')}
+                    />
+                </TouchableOpacity>
+                </View>
+        );
+    }
+    renderFalseButton = () => {
+        return (
+            <View style={{flex: 1}} >
+                <TouchableOpacity>
+                    <Image
+                        style={styles.ButtonStyles}
+                        source={require('./images/false.png')}
+                    />
+                </TouchableOpacity>
+                </View>
+        );
+    }
+    render() {
+        return (
+            <View style={styles.containerStyles}>
+                <View style={{ flex: 1 }} />
+                <View style={{ flex: 1 }} />
+
+                <View style={{ flex: 2, flexDirection: 'row' }}>
+                    {this.renderTureButton()}
+                    {this.renderFalseButton()}
+                </View>
+
+            </View>
         );
     }
 }
-
+const styles = {
+    containerStyles: {
+        flex: 5,
+        backgroundColor: '#F19D6C'
+    },
+    ButtonStyles: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: '75%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 150,
+        height: 150,
+        backgroundColor: '#F19D6C',
+    }
+}
 export default Mode1;
