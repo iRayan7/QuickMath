fbd1e60fe7bed2c71a6da07c86838284ad95567a
Rayan <ryn7des@gmail.com>
2018-03-03 21:06:58 +0300

    l

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, StatusBar, TouchableOpacity, Image } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
 
-class Leaderbord extends Component {
+export default class Leaderboard extends Component {
 
     render(){
         return (
@@ -11,4 +11,3 @@ class Leaderbord extends Component {
     }
 }
 
-export default Leaderboard;
