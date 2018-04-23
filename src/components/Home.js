464dc0cefc79ae8a241c3d7de7e8bf83db002adf
Abdulaziz AlObaili <alobaili@Abdulazizs-MacBook-Pro.local>
2018-04-23 12:41:37 +0300

    Fully redesigned Home UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,30 +10,34 @@ class Home extends Component {
                 <StatusBar hidden />
                 <Text style={styles.label}>Quick Math!</Text>
                 <View style={styles.playButtonsContainer}>
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('Mode2')}
-                    style={styles.ButtonStyles}
-                >
-                <View>
-                    <Text>➕➖➗✖️</Text>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('Mode2')}
+                        style={styles.Mode2ButtonStyle}
+                    >
+                    <View>
+                        <Text style={styles.buttonLabels}>Math Clock</Text>
+                    </View>
+                    </TouchableOpacity>
                 </View>
-                </TouchableOpacity>
+                <View>
                 <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('Mode1', {refresh: false})}
-                    style={styles.ButtonStyles}
-                >
-                    <View>
-                        <Text>✅❎</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate('QMLeaderboard')}
-                    style={styles.ButtonStyles}
-                >
-                    <View>
-                        <Text>Leaderboard</Text>
-                    </View>
-                </TouchableOpacity>
+                        onPress={() => this.props.navigation.navigate('Mode1', {refresh: false})}
+                        style={styles.Mode1ButtonStyle}
+                    >
+                        <View>
+                            <Text style={styles.buttonLabels}>Yes / No</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.leaderboardButtonContainer}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('QMLeaderboard')}
+                        style={styles.leaderboardButtonStyle}
+                    >
+                        <View>
+                            <Text style={styles.buttonLabels}>Leaderboard</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
@@ -42,27 +46,68 @@ class Home extends Component {
 }
 
 const styles = {
-    containerStyles: {
-        flex: 3,
-        backgroundColor: '#F19D6C',
+    containerStyles: { //de7a59
+        flex: 1,
+        backgroundColor: '#f2ad97',
     },
     label: {
-        marginTop: 100,
-        fontSize: 40,
+        marginTop: '45%',
+        fontSize: 50,
         textAlign: 'center',
         color: 'white',
         fontFamily: 'Avenir-Black'
     },
-    ButtonStyles: {
+    buttonLabels: {
+        fontSize: 30,
+        textAlign: 'center',
+        color: 'white',
+        fontFamily: 'Avenir-Black'
+    },
+    Mode2ButtonStyle: {
         alignItems: 'center',
         alignSelf: 'center',
-        marginTop: '75%',
+        //marginTop: '50%',
         alignItems: 'center',
         justifyContent: 'center',
-        width: 120,
-        height: 120,
-        backgroundColor: '#fff',
-        borderRadius: 100,
+        width: '100%',
+        height: 150,
+        backgroundColor: '#e38f72',
+        borderRadius: 0,
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+    },
+    Mode1ButtonStyle: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        //marginTop: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 150,
+        backgroundColor: '#e2886a',
+        borderRadius: 0,
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+    },
+    leaderboardButtonStyle: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        //marginTop: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: 130,
+        backgroundColor: '#de7a59',
+        borderRadius: 0,
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+    },
+    leaderboardButtonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
     },
     playButtonsContainer: {
         flexDirection: 'row',
